1cdaef0b17b0bbc68dfdaa4eaf7335393c813372
Secri <44509048+Secri@users.noreply.github.com>
2024-03-04 00:00:54 +0100

    Create wp_tarteaucitron.php

---
+++ wp_tarteaucitron.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Plugin d 'intégration de tarteaucitron.js
+Plugin URI: 
+Description: Intégration wordpress du plugin de consentement javascript tarteaucitron.js
+Authors: Christophe IENZER
+Version: 0.1
+Author URI: https://www.linkedin.com/in/christophe-ienzer/
+*/
+
+/* Chargement des ressources PHP pour gérer le plugin */
+require_once plugin_dir_path(__FILE__) . 'assets/functions.php';
